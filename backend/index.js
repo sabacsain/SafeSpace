9271269d31c15e1f0ad4a4d96551e14debf64644
Shan Allen Bacsain <48052318+sabacsain@users.noreply.github.com>
2024-04-18 16:25:30 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 const db = mysql.createConnection({
     host: "sql.freedb.tech",
