d2a65eaa91021a33e1d88f8d373c09a274ac4027
Mnemosyne-Grayswandir <hamormarygrizelle@gmail.com>
2024-04-22 13:48:53 +0800

    Links and animation
    - Added links for news
    - Added animations on pages

--- src/views/Communities.jsx
+++ src/views/Communities.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fade } from 'react-awesome-reveal';
 import Banner from '../components/Banner';
 import nami from '../assets/communities/nami.jpg'
 import cups from '../assets/communities/7cups.png'
@@ -38,17 +39,21 @@ const Communities = () => {
       <Banner title='Support Communities' image='/public/banner/communities.png' />
       
       <div className='mb-24'>
+      <Fade direction='right' duration={1750}>
       <CommunityCard image={nami} title="NAMI (National Alliance on Mental Illness)">
         NAMI, the National Alliance on Mental Illness, is the nation's largest  grassroots mental health organization dedicated to building better lives  for millions of Americans affected by mental illness. Through advocacy,  education, support, and public awareness, NAMI strives to improve the  lives of individuals and families impacted by mental health conditions.  With a network of over 600 local affiliates across the country, NAMI  offers support groups, educational programs, and advocacy initiatives to  promote understanding, reduce stigma, and ensure access to quality  mental health care for all. Join us in our mission to support, educate,  and advocate for individuals living with mental illness and their loved  ones.
       </CommunityCard>
-
+      </Fade>
+      <Fade direction='left' duration={1750}>
       <CommunityCard image={cups} title="7 Cups">
         7 Cups is a comprehensive online platform that serves as a beacon of support for individuals navigating the complexities of mental health challenges. At its core, 7 Cups operates as a bridge between those seeking solace and understanding and a network of compassionate, trained listeners ready to offer empathetic ears and guidance. Through its array of resources, individuals can access anonymous chat rooms, participate in forums, and engage in one-on-one messaging, all designed to provide a safe and nurturing environment for expression and connection.
       </CommunityCard>
-
+      </Fade>
+      <Fade direction='right' duration={1750}>
       <CommunityCard image={psych} title="Psychology Today">
       Psychology Today stands as a robust repository of knowledge and insight,  providing a treasure trove of articles, blogs, and resources that delve  into the intricacies of mental health, psychology, and overall  well-being. Authored by esteemed experts and professionals in the field,  each piece is meticulously crafted to offer nuanced perspectives and  practical guidance. Covering a vast array of topics ranging from anxiety  and depression to relationships and personal development, Psychology  Today serves as a beacon of enlightenment for individuals seeking to  deepen their understanding of the human mind and behavior.
       </CommunityCard>
+      </Fade>
       </div>
     </>
   );
