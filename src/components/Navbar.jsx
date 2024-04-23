e40e2d860344eddfdd251b2ec58bd98ea250bb90
QueencessMay <munsayac.queencessmay@gmail.com>
2024-04-23 16:45:46 +0800

    styled active link and logged in button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,75 @@
-import React, { useReducer } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { MainPages, LoginPages } from '../routes/paths';
-import Button from './Button';
-import sprite from '/sprite.svg';
 import { auth } from '../config/firebase';
 import { useAuthState} from 'react-firebase-hooks/auth';
 import { signOut } from 'firebase/auth';
+import Button from './Button';
+import sprite from '/sprite.svg';
 
-const Menu = props => {
+const menuList = [
+  {
+    name: 'Home', link:  MainPages.HOME,
+  },
+  {
+    name: 'About', link:  MainPages.ABOUT,
+  },
+  {
+    name: 'Services', link:  MainPages.CONSULTATION,
+    submenu: [
+      { name: 'Consultation', link: MainPages.CONSULTATION },
+      { name: 'Self-Assessment', link: MainPages.ASSESSMENT },
+    ],
+  },
+  {
+    name: 'Resources', link:  MainPages.RESOURCES,
+    submenu: [
+      { name: 'Educational Resources', link: MainPages.RESOURCES },
+      { name: 'Emergency Hotlines', link: MainPages.HOTLINES },
+      { name: 'News and Updates', link: MainPages.NEWS },
+      { name: 'Support Communities', link: MainPages.COMMUNITIES },
+      { name: 'Privacy Policy', link: MainPages.PRIVACY },
+    ],
+  },
+  {
+    name: 'Contact', link:  MainPages.CONTACT,
+  },
+];
+
+const Menu = ({ menu }) => {
+  const { pathname } = useLocation();
+  const { name, link, submenu } = menu;
+  
   return (
-    props.name ? (
+    submenu ? (
       <li className='group relative z-50'>
-        <div className='flex items-center gap-2 group-hover:text-accent transition-colors'>
-          <Link to={props.link}>{props.name}</Link>
-          <svg className='w-[14px] h-[14px] fill-secondary-200 group-hover:fill-accent group-hover:rotate-180 transition-all ease-in-out duration-300'>
+        <div className={`flex items-center gap-2 group-hover:text-accent ${(submenu.map(item => item.link)).includes(pathname) && 'text-accent'} transition-colors`}>
+          <Link to={link}>{name}</Link>
+          <svg className={`w-[14px] h-[14px] ${(submenu.map(item => item.link)).includes(pathname) ? 'fill-accent' : 'fill-secondary-200 '} group-hover:fill-accent group-hover:rotate-180 transition-all ease-in-out duration-300`}>
             <use xlinkHref={`${sprite}#dropdown`} />
           </svg>
         </div>
         <ul className='hidden group-hover:block absolute p-2 -ml-2 w-56 h-auto bg-primary rounded shadow-md'>
-          {props.children}
+          {submenu.map((current, index) => 
+            <SubMenu key={index} link={current.link}>{current.name}</SubMenu>
+          )}
         </ul>
       </li>
-      
     ) : (
-      <li className='z-50 hover:text-accent transition-colors'>
-        <Link to={props.link}>{props.children}</Link>
+      <li className={`z-50 hover:text-accent ${pathname === menu.link && 'text-accent'} transition-colors`}>
+        <Link to={menu.link}>{menu.name}</Link>
       </li>
     )
   );
-};
+};  
 
 const SubMenu = props => {
+  const { pathname } = useLocation();
+
   return (
     <li className='group/sub p-1 h-8 flex items-center'>
       <div className='bg-accent w-0 h-full group-hover/sub:w-1 rounded-sm transition-all duration-300'></div>
-      <p className='group-hover/sub:pl-2 text-secondary-200 group-hover/sub:text-accent transition-all duration-300'>
+      <p className={`group-hover/sub:pl-2 ${pathname === props.link ? 'text-accent' : 'text-secondary-200'} group-hover/sub:text-accent transition-all duration-300`}>
         <Link to={props.link}>{props.children}</Link>
       </p>
     </li>
@@ -57,6 +92,7 @@ const UserMenu = props => {
     </li>
   );
 };
+
 const Navbar = () => {
   const [user] = useAuthState(auth);
 
@@ -74,35 +110,22 @@ const Navbar = () => {
       
       <nav className='flex items-center gap-10'>
         <ul className='flex gap-10'>
-          <Menu link={MainPages.HOME}>Home</Menu>
-          <Menu link={MainPages.ABOUT}>About</Menu>
-          <Menu name="Services" link={MainPages.CONSULTATION}>
-            <SubMenu link={MainPages.CONSULTATION}>Consultation</SubMenu>
-            <SubMenu link={MainPages.ASSESSMENT}>Self-Assessment</SubMenu>
-          </Menu>
-          <Menu name="Resources" link={MainPages.RESOURCES}>
-            <SubMenu link={MainPages.RESOURCES}>Educational Resources</SubMenu>
-            <SubMenu link={MainPages.HOTLINES}>Emergency Hotlines</SubMenu>
-            <SubMenu link={MainPages.NEWS}>News and Updates</SubMenu>
-            <SubMenu link={MainPages.PRIVACY}>Privacy Policy</SubMenu>
-            <SubMenu link={MainPages.COMMUNITIES}>Support Communities</SubMenu>
-          </Menu>
-          <Menu link={MainPages.CONTACT}>Contact</Menu>
+          {menuList.map((menu, index) => 
+            <Menu key={index} menu={menu} />
+          )}
         </ul>
         
         {user ? (
-          <>
-            <ul className='flex gap-10'>
-            <Menu name={user?.email}>
+          <div className='group relative z-50'>
+            <Button>{user?.email}</Button>
+            <ul className='hidden group-hover:block absolute right-0 p-2 -ml-2 w-40 h-auto bg-primary rounded shadow-md'>
               <UserMenu link={MainPages.HOME}>Profile</UserMenu>
               <UserMenu onClick={signUserOut} link={MainPages.HOME}>Logout</UserMenu>
-            </Menu>
             </ul>
-          </>
+          </div>
         ) : (
-        <Button link={LoginPages.LOGIN}>Join Us</Button>
-        )
-        }
+          <Button link={LoginPages.LOGIN}>Join Us</Button>
+        )}
                 
       </nav>
     </header>
